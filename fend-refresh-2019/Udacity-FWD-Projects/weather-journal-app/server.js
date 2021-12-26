a7821fe21563ac6719385476f93c4d1c9da6d94e
Omar ZIdan <s-omarzidan@zewailcity.edu.eg>
2021-12-26 02:33:46 +0200

    Get & post added to server.js and get to app.js

--- fend-refresh-2019/Udacity-FWD-Projects/weather-journal-app/server.js
+++ fend-refresh-2019/Udacity-FWD-Projects/weather-journal-app/server.js
@@ -1,9 +1,16 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+const port = 3000;
 
 // Require Express to run server and routes
+const http = require('http');
+const express = require('express');
+/* Dependencies */
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +18,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+const server = app.listen(port, listening);
+function listening() {
+    console.log(`Server running on localhost: ${port}`);
+};
+
+//GET method
+app.get("/getProjectData", (req, res) => {
+    res.send(projectData);
+});
+
+//POST method
+app.post("/addToProjectData", (req, res) => {
+    //To be modified
+    console.log(req.body);
+    console.log('Post received');
+    // projectData.push(req.body);
+    //Data Parts
+    let newEntry = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        userResponse: req.body.userResponse
+    };
+    projectData.push(newEntry);
+    console.log(projectData);
+});
--- fend-refresh-2019/Udacity-FWD-Projects/weather-journal-app/website/app.js
+++ fend-refresh-2019/Udacity-FWD-Projects/weather-journal-app/website/app.js
@@ -1,5 +1,40 @@
 /* Global Variables */
-
+const apiKey = "&appid=8e0cdec5c29046e2430cfa30678d7707";
+const baseURL = "http://api.openweathermap.org/data/2.5/forecast?id=";
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+//Calling for API
+/*http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}*/
+
+//london id = 2643743
+
+//524901 is the zip code
+
+/*api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8e0cdec5c29046e2430cfa30678d7707*/
+
+//Get request using async function and fetch
+const getWeatherData = async (URL, zip, key) => {
+
+    const res = await fetch(URL + zip + key)
+    try {
+
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+//getAction function that calls getWeatherData functions with user parameters
+function performGetAction(e) {
+    let newZip = document.getElementById('zip').value;
+    getWeatherData(baseURL, newZip, apiKey);
+}
+
+
+//Event listener for generate button
+document.getElementById('generate').addEventListener('click', performGetAction);
