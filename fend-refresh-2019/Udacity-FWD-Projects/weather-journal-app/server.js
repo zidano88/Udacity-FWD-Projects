fd22718186b74292cee64a3f476be837478a3df3
Omar ZIdan <s-omarzidan@zewailcity.edu.eg>
2021-12-27 02:23:02 +0200

    get & post & update UI added

--- fend-refresh-2019/Udacity-FWD-Projects/weather-journal-app/server.js
+++ fend-refresh-2019/Udacity-FWD-Projects/weather-journal-app/server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 const port = 3000;
 
 // Require Express to run server and routes
--- fend-refresh-2019/Udacity-FWD-Projects/weather-journal-app/website/app.js
+++ fend-refresh-2019/Udacity-FWD-Projects/weather-journal-app/website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-const apiKey = "";
+const apiKey = "&appid=8e0cdec5c29046e2430cfa30678d7707";
 const baseURL = "http://api.openweathermap.org/data/2.5/forecast?id=";
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -14,27 +14,108 @@ let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 /*api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8e0cdec5c29046e2430cfa30678d7707*/
 
-//Get request using async function and fetch
+//getWeatherData is a GET request that uses async function and fetch to retrieve data from weather API
 const getWeatherData = async (URL, zip, key) => {
 
     const res = await fetch(URL + zip + key)
     try {
-
         const data = await res.json();
         console.log(data)
         return data;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+//postWeatherDataToServer is a POST function that posts data to the server
+const postWeatherDataToServer = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        // console.log(newData);
+        return newData
+        con
     } catch (error) {
         console.log("error", error);
         // appropriately handle the error
     }
 }
 
-//getAction function that calls getWeatherData functions with user parameters
-function performGetAction(e) {
-    let newZip = document.getElementById('zip').value;
-    getWeatherData(baseURL, newZip, apiKey);
+//getWeatherDatafromServer is a GET function that receives data from the server
+const getWeatherDatafromServer = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.       
+    });
+
+    try {
+        const newData = await response.json();
+        // console.log('this is received data')
+        // console.log(newData);
+        return newData
+        con
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
 }
+
+//performAction function that calls getWeatherData functions with user parameters to retrieve data from the API and then calls postWeatherDataToServer to post this received data to the server
+function performAction(e) {
+    let newZip = document.getElementById('zip').value;
+    const testZip = '4350049'
+    //Chage testZip to newZip to add user entered zip
+    getWeatherData(baseURL, newZip, apiKey)
+
+        //This part call the postWeatherDataToServer function to post retireved data from the api to the server and this data includes date and temperature and user entered feeling
+        .then(function (data) {
+            //Retrieve user entered feeling
+            let userEnteredResponse = document.getElementById('feelings').value;
+            //The following 2 variables retrieves date and temperature from the response received from weather API and they use DOT notation to access those values from the body of the response object from the API 
+            let retrievedDate = data.list[0].dt_txt;
+            let retrievedTemperature = data.list[0].main.temp;
+            //This part calls the postWeatherDataToServer function to POST data to the server
+            postWeatherDataToServer('/addToProjectData', {
+                temperature: retrievedTemperature,
+                date: retrievedDate,
+                userResponse: userEnteredResponse
+            })
+        })
+
+
+        .then(function (data) {
+            getWeatherDatafromServer('getProjectData')
+                .then(function (data) {
+
+                    // document.getElementById('toDisplayTest').innerHTML = "<img src=" + data + ">";
+
+                    let latestData = data[(data.length - 1)];
+                    let lastDate = latestData.date;
+                    let lastTemperature = latestData.temperature;
+                    let lastResponse = latestData.userResponse;
+                    document.getElementById('date').innerHTML = "Date : " + lastDate;
+                    document.getElementById('temp').innerHTML = "Temperature : " + lastTemperature;
+                    document.getElementById('content').innerHTML = "Feeling : " + lastResponse;
+                    // console.log(latestData)
+                    // console.log(latestData.date)
+                    // console.log(latestData.temperature)
+                    // console.log(latestData.userResponse)
+                    // document.getElementById('date').innerHTML = 'adsfqedoooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaao';
+                    // console.log("first object" + data.pop(1))
+                })
+
+        })
+
+
+};
 
 
 //Event listener for generate button
-document.getElementById('generate').addEventListener('click', performGetAction);
+document.getElementById('generate').addEventListener('click', performAction);
